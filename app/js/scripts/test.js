0560f085a6db163a1aed2104fe4427674eb0595d
Daria Grekova <dg94@bk.ru>
2022-05-22 16:13:57 +0300

    added burger menu

--- app/js/scripts/test.js
+++ app/js/scripts/test.js
@@ -1,3 +1,9 @@
 export function testFunction(){
-    console.log('done!');
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
 }
