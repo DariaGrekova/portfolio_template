83f2b20bda94bf42a69858ad5111ceeed641b5a0
dga <gda@n-e.ru>
2022-05-23 18:04:18 +0300

    added smooth scroll

--- app/js/scripts/burger.js
+++ app/js/scripts/burger.js
@@ -10,4 +10,29 @@ export function testFunction() {
     bg.classList.toggle('active');
     document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
   })
+
+  // scroll
+  document.querySelectorAll('a[href^="#"').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      const topOffset = 0;
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+
+      if (menu.classList.contains('active')) {
+        menuBtn.classList.remove('active');
+        menu.classList.remove('active');
+        bg.classList.remove('active');
+        document.body.style.overflow = ''
+      }
+    });
+  });
 }
